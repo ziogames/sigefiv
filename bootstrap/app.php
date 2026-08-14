7d3722a01924b274c73688b207e9ca929aabddcd
Freddy Antayhua Saavedra <ziojavanet@gmail.com>
2026-08-14 13:32:08 -0500

    Configurar HTTPS detrás de proxy

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-
+         $middleware->trustProxies(at: '*');
     $middleware->alias([
 
         'role' => \Spatie\Permission\Middleware\RoleMiddleware::class,
