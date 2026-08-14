b2387df046ad7595a419e67e86a337b9cef3372e
Freddy Antayhua Saavedra <ziojavanet@gmail.com>
2026-08-14 13:46:39 -0500

    Corregir HTTPS detrás de proxy Render

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,13 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-         $middleware->trustProxies(at: '*');
+         $middleware->trustProxies(
+    at: '*',
+    headers: Request::HEADER_X_FORWARDED_FOR |
+        Request::HEADER_X_FORWARDED_HOST |
+        Request::HEADER_X_FORWARDED_PORT |
+        Request::HEADER_X_FORWARDED_PROTO
+);
     $middleware->alias([
 
         'role' => \Spatie\Permission\Middleware\RoleMiddleware::class,
