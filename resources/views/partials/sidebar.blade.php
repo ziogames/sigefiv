c21191c44a08dc6bdeddc87bcbc1facf6d718a9b
Freddy Antayhua Saavedra <ziojavanet@gmail.com>
2026-08-16 23:38:09 -0500

    Actualiza menu lateral de asambleas

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -2,189 +2,170 @@
 
     <div class="sidebar-header border-bottom">
 
-      <div class="sidebar-brand d-flex flex-column align-items-center py-3">
+        <div class="sidebar-brand d-flex flex-column align-items-center py-3">
 
-    @if(!empty($configuracionGlobal?->logo))
+            @if (!empty($configuracionGlobal?->logo))
+                <img src="{{ asset('storage/' . $configuracionGlobal->logo) }}" alt="Logo" style="height:55px;"
+                    class="mb-2">
+            @endif
 
-        <img
-            src="{{ asset('storage/'.$configuracionGlobal->logo) }}"
-            alt="Logo"
-            style="height:55px;"
-            class="mb-2">
+            <span class="fw-bold text-white">
 
-    @endif
+                {{ $configuracionGlobal->nombre_sistema ?? 'SIGEFIV' }}
 
-    <span class="fw-bold text-white">
+            </span>
 
-        {{ $configuracionGlobal->nombre_sistema ?? 'SIGEFIV' }}
+            <small class="text-white-50">
 
-    </span>
+                {{ $configuracionGlobal->organizacion ?? '' }}
 
-    <small class="text-white-50">
+            </small>
 
-        {{ $configuracionGlobal->organizacion ?? '' }}
+        </div>
 
-    </small>
-
-</div>
-
-        <button
-    class="btn-close d-lg-none"
-    type="button"
-    onclick="toggleSidebarMovil()">
-</button>
+        <button class="btn-close d-lg-none" type="button" onclick="toggleSidebarMovil()">
+        </button>
 
     </div>
 
     <ul class="sidebar-nav" data-coreui="navigation" data-simplebar>
 
         @can('dashboard')
-<li class="nav-item">
-    <a class="nav-link" href="{{ route('dashboard') }}">
-        <i class="nav-icon cil-speedometer"></i>
-        Dashboard
-    </a>
-</li>
-@endcan
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('dashboard') }}">
+                    <i class="nav-icon cil-speedometer"></i>
+                    Dashboard
+                </a>
+            </li>
+        @endcan
 
         <li class="nav-title">
             CONTABILIDAD
         </li>
 
-      @can('reportes.index')
-<li class="nav-item">
+        @can('reportes.index')
+            <li class="nav-item">
 
-    <a
-        class="nav-link"
-        href="{{ route('reportes.index') }}">
+                <a class="nav-link" href="{{ route('reportes.index') }}">
 
-        <i class="nav-icon cil-chart"></i>
+                    <i class="nav-icon cil-chart"></i>
 
-        Reportes
+                    Reportes
 
-    </a>
+                </a>
 
-</li>
-@endcan
-
-@can('movimientos.index')
-<li class="nav-item">
-
-    <a
-        class="nav-link"
-        href="{{ route('movimientos.index') }}">
-
-        <i class="nav-icon cil-transfer"></i>
-
-        Movimientos
-
-    </a>
-
-</li>
-@endcan
-
-@can('caja.index')
-<li class="nav-item">
-
-    <a
-        class="nav-link"
-        href="{{ route('caja.index') }}">
-
-        <i class="nav-icon cil-wallet"></i>
-
-        Caja
-
-    </a>
-
-</li>
-                <li class="nav-title">
-            COMUNIDAD
-        </li>
-
-        @can('asambleas.index')
-        <li class="nav-item">
-
-            <a
-                class="nav-link"
-                href="{{ route('asambleas.index') }}"
-            >
-
-                <i class="nav-icon cil-bullhorn"></i>
-
-                Asambleas
-
-            </a>
-
-        </li>
+            </li>
         @endcan
 
-@endcan
+        @can('movimientos.index')
+            <li class="nav-item">
+
+                <a class="nav-link" href="{{ route('movimientos.index') }}">
+
+                    <i class="nav-icon cil-transfer"></i>
+
+                    Movimientos
+
+                </a>
+
+            </li>
+        @endcan
+
+        @can('caja.index')
+            <li class="nav-item">
+
+                <a class="nav-link" href="{{ route('caja.index') }}">
+
+                    <i class="nav-icon cil-wallet"></i>
+
+                    Caja
+
+                </a>
+
+            </li>
+            
+
+        @endcan
+        @can('asambleas.index')
+        <li class="nav-title">
+                COMUNIDAD
+            </li>
+
+       
+                <li class="nav-item">
+
+                    <a class="nav-link" href="{{ route('asambleas.index') }}">
+
+                        <i class="nav-icon cil-bullhorn"></i>
+
+                        Asambleas
+
+                    </a>
+
+                </li>
+            @endcan
 
         <li class="nav-title">
             ADMINISTRACIÓN
         </li>
 
-       @can('usuarios.index')
-<li class="nav-item">
-    <a class="nav-link" href="{{ route('usuarios.index') }}">
-        <i class="nav-icon cil-user"></i>
-        Usuarios
-    </a>
-</li>
-@endcan
+        @can('usuarios.index')
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('usuarios.index') }}">
+                    <i class="nav-icon cil-user"></i>
+                    Usuarios
+                </a>
+            </li>
+        @endcan
 
         @can('roles.index')
-<li class="nav-item">
-    <a class="nav-link" href="{{ route('roles.index') }}">
-        <i class="nav-icon cil-shield-alt"></i>
-        Roles
-    </a>
-</li>
-@endcan
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('roles.index') }}">
+                    <i class="nav-icon cil-shield-alt"></i>
+                    Roles
+                </a>
+            </li>
+        @endcan
 
         <li class="nav-divider"></li>
 
-     @can('configuracion')
-<li class="nav-item">
+        @can('configuracion')
+            <li class="nav-item">
 
-    <a
-        class="nav-link"
-        href="{{ route('configuracion.index') }}">
+                <a class="nav-link" href="{{ route('configuracion.index') }}">
 
-        <i class="nav-icon cil-settings"></i>
+                    <i class="nav-icon cil-settings"></i>
 
-        Configuración
+                    Configuración
 
-    </a>
+                </a>
 
-</li>
-@endcan
+            </li>
+        @endcan
 
-@can('categorias.index')
-<li class="nav-item">
+        @can('categorias.index')
+            <li class="nav-item">
 
-    <a
-        class="nav-link"
-        href="{{ route('categorias.index') }}">
+                <a class="nav-link" href="{{ route('categorias.index') }}">
 
-        <i class="nav-icon cil-folder"></i>
+                    <i class="nav-icon cil-folder"></i>
 
-        Categorías
+                    Categorías
 
-    </a>
+                </a>
 
-</li>
-@endcan
+            </li>
+        @endcan
 
 
         @can('bitacora.index')
-<li class="nav-item">
-    <a class="nav-link" href="{{ route('bitacora.index') }}">
-        <i class="nav-icon cil-notes"></i>
-        Bitácora
-    </a>
-</li>
-@endcan
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('bitacora.index') }}">
+                    <i class="nav-icon cil-notes"></i>
+                    Bitácora
+                </a>
+            </li>
+        @endcan
         <li class="nav-divider"></li>
 
         <li class="nav-item mt-auto">
@@ -193,9 +174,7 @@
 
                 @csrf
 
-                <button
-                    type="submit"
-                    class="nav-link border-0 bg-transparent text-start w-100">
+                <button type="submit" class="nav-link border-0 bg-transparent text-start w-100">
 
                     <i class="nav-icon cil-account-logout"></i>
 
@@ -214,42 +193,37 @@
         </li>
 
         <li class="nav-item">
-           <a
-    class="nav-link"
-    href="{{ route('perfil.index') }}">
+            <a class="nav-link" href="{{ route('perfil.index') }}">
 
-    <i class="nav-icon cil-user"></i>
+                <i class="nav-icon cil-user"></i>
 
-    Mi Cuenta
+                Mi Cuenta
 
-</a>
+            </a>
         </li>
 
     </ul>
 
     <div class="sidebar-footer border-top d-none d-lg-flex">
 
-        <button
-            class="sidebar-toggler"
-            type="button"
-            data-coreui-toggle="unfoldable">
+        <button class="sidebar-toggler" type="button" data-coreui-toggle="unfoldable">
         </button>
 
     </div>
 
 </div>
 <script>
-function toggleSidebarMovil() {
-    const sidebar = document.getElementById('sidebar');
+    function toggleSidebarMovil() {
+        const sidebar = document.getElementById('sidebar');
 
-    if (!sidebar) {
-        return;
+        if (!sidebar) {
+            return;
+        }
+
+        const instancia = coreui.Sidebar.getInstance(sidebar);
+
+        if (instancia) {
+            instancia.toggle();
+        }
     }
-
-    const instancia = coreui.Sidebar.getInstance(sidebar);
-
-    if (instancia) {
-        instancia.toggle();
-    }
-}
 </script>
